7f18d6ff9c59a905571557b19e69ccfe75656134
ali-a-a <ali.abbasialaei@snapp.cab>
2022-03-09 21:14:13 -0800

    Add publish test

--- go.mod
+++ go.mod
@@ -3,16 +3,17 @@ module github.com/ali-a-a/gophermq
 go 1.17
 
 require (
-	github.com/go-resty/resty/v2 v2.7.0
 	github.com/google/uuid v1.1.2
 	github.com/knadh/koanf v1.4.0
 	github.com/labstack/echo/v4 v4.7.0
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cobra v1.3.0
+	github.com/stretchr/testify v1.7.0
 	go.uber.org/automaxprocs v1.4.0
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/structs v1.1.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
@@ -22,6 +23,7 @@ require (
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/mapstructure v1.4.3 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
@@ -30,4 +32,5 @@ require (
 	golang.org/x/sys v0.0.0-20211205182925-97ca703d548d // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
---
+++ internal/app/gophermq/broker/broker_test.go
@@ -0,0 +1,59 @@
+package broker
+
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestGopherMQ_Publish(t *testing.T) {
+	gm := NewGopherMQ(MaxPending(1))
+
+	subject := "test"
+	data := "data"
+
+	sub, err := gm.Subscribe(subject, func(e Event) error {
+		assert.NoError(t, e.Error())
+		assert.Equal(t, subject, e.Subject())
+		assert.Equal(t, []byte(data), e.Data())
+
+		return nil
+	})
+
+	assert.NoError(t, err)
+	assert.Equal(t, subject, sub.subj)
+	assert.NotEmpty(t, sub.id)
+
+	err = gm.Publish(subject, []byte(data))
+	assert.NoError(t, err)
+	assert.Empty(t, gm.queue[subject])
+
+	time.Sleep(100 * time.Millisecond)
+
+	gm.opts.MaxPending = -1
+
+	err = gm.Publish(subject, []byte(data))
+	assert.ErrorIs(t, err, ErrMaxPending)
+
+	gm.opts.MaxPending = 1
+
+	err = gm.Publish("bad subject", []byte(data))
+	assert.ErrorIs(t, err, ErrBadSubject)
+
+	_, err = gm.Subscribe(subject, func(e Event) error {
+		return errors.New("should be failed")
+	})
+
+	assert.NoError(t, err)
+
+	err = gm.Publish(subject, []byte(data))
+	assert.Error(t, err)
+	assert.NotEqual(t, err, ErrMaxPending)
+	assert.NotEmpty(t, gm.queue[subject])
+
+	err = gm.Publish(subject, []byte(data))
+	err = gm.Publish(subject, []byte(data))
+	assert.Error(t, err)
+	assert.Equal(t, err, ErrMaxPending)
+}
